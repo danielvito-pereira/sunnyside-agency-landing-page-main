1d21d8840d4f23c406e67bce4bad790aafdef080
danielvito-pereira <danielvitor081@gmail.com>
2023-01-23 12:02:13 -0300

    ajustes nas sections versão desktop

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+/* Quando clico no .button, .nav TOGGLE 'activo' */
+const button = document.querySelector('button');/*me permite selecionar os dois elementos  */
+const nav    = document.querySelector('nav');
+
+button.addEventListener('click',()=>{
+  nav.classList.toggle('activo')
+})
